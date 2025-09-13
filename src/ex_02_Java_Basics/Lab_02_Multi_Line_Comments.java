39af76b065de7f03b54af24ab1cdf1fea866e972
divyaganesh1996 <divya.ganesh9096@gmail.com>
2025-09-13 11:34:48 +0530

    13 sep newly added

---
+++ src/ex_02_Java_Basics/Lab_02_Multi_Line_Comments.java
@@ -0,0 +1,20 @@
+package ex_02_Java_Basics;
+/*
+* name-divya
+* age-28
+*
+* */
+
+public class Lab_02_Multi_Line_Comments {
+    /*
+    *
+    *
+    *
+    * */
+
+    public static void main(String[] args) {
+        /*
+        *
+        * */
+    }
+}
