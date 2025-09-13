08baa19c6c351e44d88b1f22a167d1328d0786a8
divyaganesh1996 <divya.ganesh9096@gmail.com>
2025-09-13 10:58:41 +0530

    13 sep file

---
+++ src/ex_02_Java_Basics/MMmain.java
@@ -0,0 +1,11 @@
+package ex_02_Java_Basics;
+
+public class MMmain {
+
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+    public static void main(String args) {
+        System.out.println("hi");
+    }
+}
