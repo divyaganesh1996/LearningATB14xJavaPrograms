79d41e57a231e00cfa505ea7c6e7a42e02610385
divyaganesh1996 <divya.ganesh9096@gmail.com>
2025-10-12 16:06:30 +0530

    Variable_Declaration_and_Initialization

---
+++ src/Assesment_01/Variable_Declaration_and_Initialization.java
@@ -0,0 +1,16 @@
+package Assesment_01;
+
+public class Variable_Declaration_and_Initialization {
+    public static void main(String[] args) {
+        int a = 32;
+        double b = 1234.456;
+        boolean status = false;
+        char grade = 'A';
+        System.out.println("int data type and value of a is :"+a);
+        System.out.println("double data type and value of b is :"+b);
+        System.out.println("boolean data type and value of status is :"+status);
+        System.out.println("char data type and value of grade is :"+grade);
+
+
+    }
+}
